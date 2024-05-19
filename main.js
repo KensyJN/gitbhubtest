222b25b626e39628c1642d36b9c25edd694e99cc
KensyJN <kensyjordan@gmail.com>
2024-05-19 14:59:34 -0400

    change

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+  // Generate a random color
+  var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  // Apply the random color as the background color of the body
+  document.body.style.backgroundColor = randomColor;
+});
